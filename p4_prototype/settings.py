f811dc612461d9249eeaacc11035ca29963b2f04
Eric Pineda <ericvpineda@berkeley.edu>
2021-07-29 17:14:35 -0700

    fixed bugs

--- p4_prototype/settings.py
+++ p4_prototype/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9ika-etbk+#-u&tns)2)l!+uf!j^c3z0yoi9fb-hed2nu9lv98'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["escape-in-time.herokuapp.com/", "127.0.0.1", "*"]
 
@@ -67,7 +67,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
