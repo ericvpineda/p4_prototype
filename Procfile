6f7288b82dee5ddd264ceb3fb5bc320fff0017b5
Eric Pineda <ericvpineda@berkeley.edu>
2021-07-30 20:33:14 -0700

    updated procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: python website/manage.py runserver 0.0.0.0:$PORT
+release: python manage.py migrate
+web: daphne p4_prototype.asgi:application --port $PORT --bind 0.0.0.0 -v2
+chatworker: python manage.py runworker --settings=p4_prototype.settings -v2
--- p4_prototype/settings.py
+++ p4_prototype/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -159,3 +160,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
