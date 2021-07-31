2ef85addd16952f4511d914ebe09d090eea5d15f
Eric Pineda <ericvpineda@berkeley.edu>
2021-07-30 21:28:02 -0700

    attempt wss in routing.py

--- shared_easel/routing.py
+++ shared_easel/routing.py
@@ -2,5 +2,5 @@ from django.conf.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws$', consumers.DrawConsumer.as_asgi())
+    re_path(r'wss$', consumers.DrawConsumer.as_asgi())
 ]
