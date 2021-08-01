713c38301f570df0dce6977ad4ffe1a86525b8cc
Eric Pineda <ericvpineda@berkeley.edu>
2021-07-31 23:07:53 -0700

    local host connection success

--- shared_easel/static/js/login.js
+++ shared_easel/static/js/login.js
@@ -1,96 +1,77 @@
-// var loc = window.location;
-// var wsStart = 'ws://'
-// if (loc.protocol == 'https:') {
-//     wsStart = 'wss://'
-// }
-// var endpoint = wsStart + loc.host + loc.pathname
-
-// var socket = new WebSocket(endpoint)
-
-// class User {
-
-//     constructor(uid, name, img) {
-//         this.uid = uid;
-//         this.name = name;
-//         this.img = img;
-//     }
-// }
-
-// // SOCKET FUNCTIONS
-
-// socket.onopen = function (e) {
-//     console.log("open", e)
-// }
-
-// socket.onmessage = function(msg) {
-//     var rec = JSON.parse(msg.data);
-//     if (rec.ready) {
-//         if (location.href.indexOf("login") > -1) {
-//             location.href = "/game";
-//             // console.log('SENDINGS TO LOGIN')
-//         } else {
-//             // console.log('SENDING TO MOBILE')
-//             location.href = "/individualGameScreen";
-//             socket.send(JSON.stringify(all_users));
-//         }
-//     } else {
-//         addUserToScreen(rec.name, rec.src);
-//         const user = new User(rec.uid, rec.name, rec.src)
-//         all_users.push(user)
-//     }
-// }
-
-// socket.oneerror = function(e) {
-//     console.log("error", e)
-// }
-
-// socket.onclose = function(e) {
-//     console.log('close', e)
-//     // console.error('ERROR: CHAT CLOSED :(')
-// }
+var loc = window.location;
+var wsStart = loc === 'https:' ? 'wss://' : 'ws://'
+var endpoint = wsStart + loc.host + '/ws';
+var socket = new WebSocket(endpoint)
 
 
-// // VARIABLES 
+// SOCKET FUNCTIONS
 
-// const $users = $('#users')
-// const $profilePics = $('profile-pics')
-// const all_users = []
+socket.onopen = function (e) {
+    console.log("open", e)
+}
 
-// // FUNCTIONS
+socket.onmessage = function(msg) {
+    var rec = JSON.parse(msg.data);
+    if (rec.ready) {
+        if (loc.href.indexOf("login") > -1) {
+            location.href = "/game";
+        } else {
+            location.href = "/individualGameScreen";
+        }
+    } else if (rec.name) {
+        addUserToScreen(rec.name, rec.src);
+        const user = new User(rec.uid, rec.name, rec.src)
+        all_users.push(user)
+    }
+}
 
-// $('#add-user').click(function (event) {
-//     event.preventDefault();
-//     const name = $('#name').val()
-//     const src = $('input[name="profile-pic"]:checked').val()
-//     const uid = $('#uid').text()
-//     if (src === undefined || name === "") {
-//         $('#error').toggle()
-//         $('#error').fadeOut(3000);
-//     } else {
-//         socket.send("{ \"name\" : \"" + name + "\", \"src\" : \"" + src + "\", \"uid\" : \"" + uid + "\" }");
-//         $('#add-user').attr('disabled', 'disabled')
-//     }
-// })
+socket.oneerror = function(e) {
+    console.log("error", e)
+}
 
-// $('#ready').click(function (event) {
-//     event.preventDefault();
-//     socket.send("{ \"ready\" : \"" + true + "\"}");
-// })
+socket.onclose = function(e) {
+    console.log('close', e)
+    // console.error('ERROR: CHAT CLOSED :(')
+}
 
-// function addUserToScreen(name, src) {
 
-//     if (name !== undefined && src !== undefined) {
-//         const li = document.createElement('li');
-//         const img = document.createElement('img');
-//         const div = document.createElement('div');
-//         const h3 = document.createElement('h3');
-    
-//         h3.innerText = name;
-//         img.src = src;
-//         div.append(img, h3);
-//         div.classList.add('user-card')
-//         li.append(div)
-//         $users.append(li);
-//     }
-   
-// }
+// VARIABLES 
+
+const $users = $('#users')
+const $profilePics = $('profile-pics')
+const all_users = []
+
+// FUNCTIONS
+
+$('#add-user').click(function (event) {
+    event.preventDefault();
+    const name = $('#name').val()
+    const src = $('input[name="profile-pic"]:checked').val()
+    const uid = $('#uid').text()
+    if (src === undefined || name === "") {
+        $('#error').toggle()
+        $('#error').fadeOut(3000);
+    } else {
+        socket.send("{ \"name\" : \"" + name + "\", \"src\" : \"" + src + "\", \"uid\" : \"" + uid + "\" }");
+        $('#add-user').attr('disabled', 'disabled')
+    }
+})
+
+$('#ready').click(function (event) {
+    event.preventDefault();
+    socket.send("{ \"ready\" : \"" + true + "\"}");
+})
+
+function addUserToScreen(name, src) {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    const div = document.createElement('div');
+    const h3 = document.createElement('h3');
+
+    h3.innerText = name;
+    img.src = src;
+    div.append(img, h3);
+    div.classList.add('user-card')
+    li.append(div)
+    $users.append(li);
+}
--- shared_easel/urls.py
+++ shared_easel/urls.py
@@ -1,11 +1,11 @@
 from . import views
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path 
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login', views.login, name='login'),
     path('game', views.game, name='game'),
     path('add-users', views.add_users, name='add-users'),
+    path('individualGameScreen', views.individualGameScreen, name='individualGameScreen')
 ]
 
