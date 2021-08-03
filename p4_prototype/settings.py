2b63a1b204c08f48459d2deb32370aabfa5ba532
Eric Pineda <ericvpineda@berkeley.edu>
2021-08-02 19:37:16 -0700

    settings redis

--- p4_prototype/settings.py
+++ p4_prototype/settings.py
@@ -30,10 +30,10 @@ ALLOWED_HOSTS = ["escape-in-time.herokuapp.com/", "127.0.0.1", "*"]
 
 CHANNEL_LAYERS = {
     'default' : {
-        'BACKEND' : 'channels_redis.core.RedisChannelLayer',
-        'CONFIG' : {
-            "hosts" : [('127.0.0.1', 6379)]
-        },
+        'BACKEND' : 'channels.layers.InMemoryChannelLayer',
+        # 'CONFIG' : {
+        #     "hosts" : [('127.0.0.1', '6379')]
+        # },
     },
 }
 
