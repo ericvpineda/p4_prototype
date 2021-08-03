3f67995a0534a353155ed10e73cd429d325d8e5b
Eric Pineda <ericvpineda@berkeley.edu>
2021-08-02 20:13:05 -0700

    revert settings channel_layers

--- p4_prototype/settings.py
+++ p4_prototype/settings.py
@@ -29,11 +29,11 @@ DEBUG = False
 ALLOWED_HOSTS = ["escape-in-time.herokuapp.com/", "127.0.0.1", "*"]
 
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
+    'default' : {
+        'BACKEND' : 'channels.layers.InMemoryChannelLayer',
+        # 'CONFIG' : {
+        #     "hosts" : [('127.0.0.1', '6379')]
+        # },
     },
 }
 
